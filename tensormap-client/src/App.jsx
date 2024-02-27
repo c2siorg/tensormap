8a6f7595fdcf1e5ce6acd3f031e8f4b91707f974
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-02-27 10:51:03 +0530

    client end compiling

---
+++ tensormap-client/src/App.jsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+// import Layout from "./components/Layout/Layout";
+import Home from "./containers/Home/Home";
+import DataUpload from "./containers/DataUpload/DataUpload";
+import DataProcess from "./containers/DataProcess/DataProcess";
+import DeepLearning from "./containers/DeepLearning/DeepLearning";
+import * as urls from "./constants/Urls";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={urls.HOME_URL} element={<Home />} />
+                    <Route path={urls.DATA_UPLOAD_URL} element={<DataUpload />} />
+                    <Route path={urls.DATA_PROCESS_URL} element={<DataProcess />} />
+                    <Route path={urls.DEEP_LEARN_URL} element={<DeepLearning />} />
+                    <Route exact path="/" element={<Navigate to={urls.HOME_URL} />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+}
+
+export default App;

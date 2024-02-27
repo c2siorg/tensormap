58083d48b936d3f429194a419ec0a9964979e03b
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-02-27 10:51:03 +0530

    client end compiling

--- tensormap-client/src/services/ModelServices.js
+++ tensormap-client/src/services/ModelServices.js
@@ -1,73 +1,65 @@
-import axios from "../shared/Axios";
-import * as urls from "../constants/Urls";
-import * as strings from "../constants/Strings";
+import axios from '../shared/Axios';
+import * as urls from '../constants/Urls';
+import * as strings from '../constants/Strings';
 
-export const validateModel = async (data) => {
-    return axios.post(urls.BACKEND_VALIDATE_MODEL, data)
-      .then(resp => {
-        return resp.data
-      })
-      .catch(err => {
-        console.error(err);
-        if (err.response && err.response.data){
-          return err.response.data
-        }else{
-          return {success:false,message:"Unknown error occured"}
-        }
-        
-      });
-  };
+export const validateModel = async (data) => axios.post(urls.BACKEND_VALIDATE_MODEL, data)
+  .then((resp) => resp.data)
+  .catch((err) => {
+    console.error(err);
+    if (err.response && err.response.data) {
+      return err.response.data;
+    }
+    return { success: false, message: 'Unknown error occured' };
+  });
 
-export const getAllModels = async () => {
-    return axios.get(urls.BACKEND_GET_ALL_MODELS)
-      .then(resp => {
-        if (resp.data.success === true) {
-          return resp.data.data
-        }
-        return [];
-      }).catch(err =>{
-        console.error(err);
-        throw(err)
-      })
-  };
+export const getAllModels = async () => axios.get(urls.BACKEND_GET_ALL_MODELS)
+  .then((resp) => {
+    if (resp.data.success === true) {
+      return resp.data.data;
+    }
+    return [];
+  }).catch((err) => {
+    console.error(err);
+    throw (err);
+  });
 
-export const download_code = async (model_name) =>  {
-  const data = {"model_name": model_name}
+export const download_code = async (model_name) => {
+  const data = { model_name };
   return axios.post(urls.BACKEND_DOWNLOAD_CODE, data)
-    .then(resp => {
-        const link = document.createElement("a");
-        link.href = window.URL.createObjectURL(new Blob([resp.data], { type: "application/octet-stream" }));
-        link.download = data.model_name + strings.MODEL_EXTENSION;
+    .then((resp) => {
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(new Blob([resp.data], { type: 'application/octet-stream' }));
+      link.download = data.model_name + strings.MODEL_EXTENSION;
 
-        document.body.appendChild(link);
+      document.body.appendChild(link);
 
-        link.click();
+      link.click();
 
-      setTimeout(function () {
+      setTimeout(() => {
         window.URL.revokeObjectURL(link.href);
         document.body.removeChild(link);
       }, 200);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-      throw err
+      throw err;
     });
 };
 
 export const runModel = async (modelName) => {
   const data = {
-    model_name: modelName
+    model_name: modelName,
   };
 
   return axios.post(urls.BACKEND_RUN_MODEL, data)
-    .then(resp => {
+    .then((resp) => {
       if (resp.data.success === true) {
         return resp.data.message;
       }
-      console.log(resp.data.message)
+      console.log(resp.data.message);
       throw new Error(resp.data);
     })
-    .catch(err => {
-      throw err
-    })
-}
+    .catch((err) => {
+      throw err;
+    });
+};

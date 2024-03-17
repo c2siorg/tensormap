20f96c74025dc3ed76f0c00f3e5418e75cea25d1
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-03-17 16:30:25 +0530

    fix: model templates

--- tensormap-server/templates/code-templates/linear-regression-all-float.py
+++ tensormap-server/templates/code-templates/linear-regression-all-float.py
@@ -8,20 +8,18 @@ import yaml
 
 def deep_learning_model():
     features = pd.read_csv('{{data.dataset.file_name}}')
+    features.dropna(inplace=True)
 
     # Split Training and testing sets
-    training_features = features.sample(frac={{data.dataset.training_split}}, random_state=200)
-    training_labels = training_features.pop('{{data.dataset.target_field}}')
+    X = features.drop('{{data.dataset.target_field}}', axis=1)
+    y = features['{{data.dataset.target_field}}']
 
-    testing_features = features.drop(training_features.index)
-    testing_labels = testing_features.pop('{{data.dataset.target_field}}')
-
-    x_training, y_training = (training_features.to_numpy(), training_labels.to_numpy())
-    x_testing, y_testing = (testing_features.to_numpy(), testing_labels.to_numpy())
-
-    # Preprocessing - scaling
-    x_training = x_training / np.linalg.norm(x_training)
-    x_testing = x_testing / np.linalg.norm(x_testing)
+    split_index = int(len(X) * {{data.dataset.training_split}} / 100)
+    
+    x_training = X[:split_index]
+    y_training = y[:split_index]
+    x_testing = X[split_index:]
+    y_testing = y[split_index:]
 
     json_string = json.dumps(yaml.load(open('{{data.dl_model.json_file}}')))
     model = tf.keras.models.model_from_json(json_string, custom_objects=None)

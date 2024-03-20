91b6a977e75c6d478875efc55d344c0ac01c574a
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-03-21 00:10:41 +0530

    feature: delete dataset

--- tensormap-client/src/components/Upload/FilesList/File/File.jsx
+++ tensormap-client/src/components/Upload/FilesList/File/File.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Segment } from 'semantic-ui-react';
-
+import React from "react";
+import { Segment, Button } from "semantic-ui-react";
+import { deleteFile } from "../../../../services/FileServices";
+import { useState } from "react";
 function File(props) {
-  return (
-    <div>
-      <Segment.Group horizontal raised>
-        <Segment>{props.savedFileName}</Segment>
-        <Segment>{props.savedFileType}</Segment>
-      </Segment.Group>
-    </div>
-  );
+
+    const deleteFileHandeler = async () => {
+      const a = await deleteFile(props.savedFileId)
+      props.setRefresh(!props.refresh)
+    } 
+    return (
+        <div>
+            <Segment.Group horizontal raised>
+                <Segment>{props.savedFileName}</Segment>
+                <Segment>{props.savedFileType}</Segment>
+                <Segment style={{ textAlign: "right" }}>
+                    <Button negative onClick={deleteFileHandeler}>Delete Dataset</Button>
+                </Segment>
+            </Segment.Group>
+        </div>
+    );
 }
 
 export default File;

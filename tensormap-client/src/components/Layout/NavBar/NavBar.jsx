1f999c3ca0a85e68e8bc0c921134a50181c718ed
Saatwik Vasishtha <saatwik.vasishtha@gmail.com>
2024-05-30 09:03:40 +0530

    nav-bar, logo

--- tensormap-client/src/components/Layout/NavBar/NavBar.jsx
+++ tensormap-client/src/components/Layout/NavBar/NavBar.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
-
+import { Menu } from "semantic-ui-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import * as constants from "../../../constants/Strings";
+import * as urls from "../../../constants/Urls";
 function NavBar() {
-  return (
-    <div>
-      <Menu stackable size="huge">
-        <Menu.Item>
-          <img src="https://react.semantic-ui.com/logo.png" alt="logo" />
-        </Menu.Item>
+    const location = useLocation();
 
-        <Menu.Item
-          name="TensorMap"
-          active
-          style={{ fontWeight: 'bold' }}
-        />
-      </Menu>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const handleItemClick = (name) => {
+        if (name === constants.HOME_SIDEBAR) {
+            navigate(urls.HOME_URL);
+        } else if (name === constants.DATA_UPLOAD_SIDEBAR) {
+            navigate(urls.DATA_UPLOAD_URL);
+        } else if (name === constants.DATA_PROCESS_SIDEBAR) {
+            navigate(urls.DATA_PROCESS_URL);
+        } else {
+            navigate(urls.DEEP_LEARN_URL);
+        }
+    };
+    return (
+        <div>
+            <Menu stackable size="large">
+                <Menu.Item
+                    name={constants.HOME_SIDEBAR}
+                    active={location.pathname === urls.HOME_URL}
+                    onClick={() => handleItemClick(constants.HOME_SIDEBAR)}
+                    style={{ fontWeight: "bold" }}
+                >
+                    <img src="/favicon.png" alt="logo" />
+                    TensorMap
+                </Menu.Item>
+                <Menu.Menu position="right">
+                    <Menu.Item
+                        name={constants.DATA_UPLOAD_SIDEBAR}
+                        active={location.pathname === urls.DATA_UPLOAD_URL}
+                        onClick={() => handleItemClick(constants.DATA_UPLOAD_SIDEBAR)}
+                        style={{ fontWeight: "bold" }}
+                    />
+                    <Menu.Item
+                        name={constants.DATA_PROCESS_SIDEBAR}
+                        active={location.pathname === urls.DATA_PROCESS_URL}
+                        onClick={() => handleItemClick(constants.DATA_PROCESS_SIDEBAR)}
+                        style={{ fontWeight: "bold" }}
+                    />
+                    <Menu.Item
+                        name={constants.DEEP_LEARNING_SIDEBAR}
+                        active={location.pathname === urls.DEEP_LEARN_URL}
+                        onClick={() => handleItemClick(constants.DEEP_LEARNING_SIDEBAR)}
+                        style={{ fontWeight: "bold" }}
+                    />
+                </Menu.Menu>
+            </Menu>
+        </div>
+    );
 }
 
 export default NavBar;
--- tensormap-client/tests/components/Layout/Sidebar.test.jsx
+++ tensormap-client/tests/components/Layout/Sidebar.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import Sidebar from '../DragAndDropCanvas/Sidebar';
+import Sidebar from '../../../src/components/DragAndDropCanvas/Sidebar';
 import '@testing-library/jest-dom';
 
 describe('<Sidebar />', () => {

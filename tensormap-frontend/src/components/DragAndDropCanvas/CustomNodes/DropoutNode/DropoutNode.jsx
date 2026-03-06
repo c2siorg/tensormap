b346839b3e5b89108d296cfc7effbd8551327ac4
Sickwoman <msick4094@gmail.com>
2026-03-06 12:36:49 +0530

    fix: add ReactFlow handles and resolve frontend lint issues

--- tensormap-frontend/src/components/DragAndDropCanvas/CustomNodes/DropoutNode/DropoutNode.jsx
+++ tensormap-frontend/src/components/DragAndDropCanvas/CustomNodes/DropoutNode/DropoutNode.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import { Handle, Position } from "reactflow";
 import PropTypes from "prop-types";
 
-function DropoutNode({ data, id }) {
+function DropoutNode({ data }) {
   return (
-    <div className="bg-white border rounded shadow p-2">
-      <strong>Dropout</strong>
-      <div>rate: {data?.params?.rate || 0.5}</div>
+    <div className="bg-white border rounded shadow px-3 py-2 text-xs">
+
+      <Handle type="target" position={Position.Left} />
+
+      <div className="font-semibold">Dropout</div>
+      <div>Rate: {data?.rate ?? 0.5}</div>
+
+      <Handle type="source" position={Position.Right} />
+
     </div>
   );
 }
 
 DropoutNode.propTypes = {
-  data: PropTypes.object,
-  id: PropTypes.string,
+  data: PropTypes.object
 };
 
 export default DropoutNode;
